8635695b871b58ae2cb547b971674b0fff8c3017
Rowyn Mathieu <110481471+RowynM@users.noreply.github.com>
2022-08-03 17:44:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//August 3, 2022
+// Number variables task
+let num = 15;
+let add30 = num + 30;
+console.log(add30);
+
+let subtract6 = num - 6;
+console.log(subtract6);
+
+let multiply4 = num * 4;
+console.log(multiply4);
+
+let divide3 = num / 3;
+console.log(divide3);
+
+let mod7 = num % 7;
+console.log(mod7);
+
+
+
+//String variables task
+
+let city = "Toronto";
+let country = "Canada";
+let location = city + ", " + country
+
+console.log(location);
+
+console.log(city[1], country[1], location[1]);
+
+console.log(location.toUpperCase());
+console.log(location.toLowerCase());
